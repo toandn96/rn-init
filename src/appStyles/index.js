5750d5f26e42c0f21276535712e60fd13cc403d7
toandn96 <toandn96@gmail.com>
2021-03-16 16:34:34 +0700

    update

--- src/appStyles/index.js
+++ src/appStyles/index.js
@@ -1,5 +1,4 @@
 import {AppColors} from './Colors';
 import {AppFonts} from './Fonts';
-import * as AppDimensions from './Dimensions';
 
-export {AppColors, AppFonts, AppDimensions};
+export {AppColors, AppFonts};
